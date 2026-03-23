2a31f2414b1346b10e413c4d110e737c4bb0f5ba
SmitVasoya3110 <68841925+SmitVasoya3110@users.noreply.github.com>
2026-03-23 21:52:31 +0530

    Each client gets own queue: resolving slow client issue

---
+++ app/websocket/client.py
@@ -0,0 +1,22 @@
+import asyncio
+
+from fastapi import WebSocket
+
+
+class ClientConnection:
+    def __init__(self, websocket:WebSocket) -> None:
+        self.websocket = websocket
+        self.queue = asyncio.Queue(maxsize=100)
+        self.task = None
+        self.active = True
+        
+    
+    async def sender(self):
+        try:
+            while self.active:
+                message = await self.queue.get()
+                await self.websocket.send_json(message)
+                
+        except Exception:
+            self.active = False
+            
--- app/websocket/manager.py
+++ app/websocket/manager.py
@@ -2,22 +2,38 @@ from fastapi import WebSocket
 from typing import List
 import asyncio
 
+from app.websocket.client import ClientConnection
+
 
 class ConnectionManager:
 
     def __init__(self):
-        self.active_connections: List[WebSocket] = []
+        self.clients: List[ClientConnection] = []
+        
 
     async def connect(self, websocket: WebSocket):
         await websocket.accept()
-        self.active_connections.append(websocket)
+        client = ClientConnection(websocket)
+        self.clients.append(client)
+        
+        client.task = asyncio.create_task(client.sender())
 
     def disconnect(self, websocket: WebSocket):
-        self.active_connections.remove(websocket)
+        for client in self.clients:
+            if client.websocket == websocket:
+                client.active = False
+                self.clients.remove(client)
+                break
 
     async def broadcast(self, message: dict):
-        for connection in self.active_connections:
+        dead_clients = []
+        
+        for client in self.clients:
             try:
-                await connection.send_json(message)
-            except Exception:
-                self.disconnect(connection)
+                client.queue.put_nowait(message)
+            except asyncio.QueueFull:
+                dead_clients.append(client)
+                
+        for client in dead_clients:
+            client.active = False
+            self.clients.remove(client)
